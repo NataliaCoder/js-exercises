d3946e0b3ee395f4d03a680637dab73d8307df1b
Natalia <n5@mail.ru>
2020-06-27 12:34:30 +0100

    no changes

--- week-6/Homework/mandatory/3-DOM-practice/main.js
+++ week-6/Homework/mandatory/3-DOM-practice/main.js
@@ -3,36 +3,30 @@ console.log("Testing JS file loaded!")
 // Task 1
 
 // Without changing any of the HTML or CSS, update the <section> tags so that they have white backgrounds.
+
 section = document.querySelectorAll("section");
-// for (i = 0; i < section.length; i++) {
-//     section[i].style.backgroundColor = "white";
-// }
-
-section.forEach(myFunction);
-
-function myFunction(element) {
-    element.style.backgroundColor = "white";
-} ;
+section.forEach((element) => (element.style.backgroundColor = "white"));
 
 // Task 2
 // Without changing any of the HTML or CSS, update the images on the page so that they are all centered.
 // Hint: look at the CSS to see if there are any classes already written which you can use.
 
-image = document.querySelectorAll("img");
-image.forEach(myFunc);
-function myFunc(elemen) {
-elemen.className = ".content-title";
-};
-
+document.querySelectorAll("img").forEach((element) => (element.className = "content-title"));
 
 // Task 3
 
 // Google the date of birth and death of each of the people on the page. Without changing any of the HTML or CSS, add this in a paragraph to the end of their <section>.
-9 December 1906 
-1 January 1992  
 
-26 August 1918 
-24 February 2020  
+let index = 0;
+let years = [
+    "9 December 1906 - 1 January 1992",  
+    "26 August 1918 - 24 February 2020",  
+    "10 December 1815 - 27 November 1852"
+];
 
-10 December 1815
-27 November 1852
+document.querySelectorAll("section").forEach((element) => {
+let paragraph = document.createElement("p");
+paragraph.textContent = years[index];
+element.append(paragraph);
+index++;
+});
